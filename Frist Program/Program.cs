de325973dab75c63672c777b1b286cc5a670080d
PC <PC@DESKTOP-CGG60AM>
2019-04-12 19:13:40 +0400

    Add project files.

---
+++ Frist Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frist_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wolrd");
+            Console.Write("I'm AbDuLlAh");
+            Console.ReadKey();
+
+        }
+    }
+}
